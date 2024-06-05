ea67dde1cb0cced650ba0527dae58e85944d3acb
lulamile99 <lulabenni45@gmail.com>
2024-06-05 04:22:43 +0200

    Using use states for props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Person from './components/Person'
-const name = "Lulamile"
+import { useState } from 'react'
+
 function App() {
+  //let name = "Lulamile"
+  const [name, setName] = useState('Lulamile');
   return (
     <div className='App'>
+      <input type="text" onChange={(e) => {setName(e.target.value)}}/>
       <Person name={name} role="Student"/>
       <Person name="Benni"/>
       <Person/>
