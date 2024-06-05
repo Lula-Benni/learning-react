454d23dcc886d7b5829092d2c6382ca13fbcfad8
lulamile99 <lulabenni45@gmail.com>
2024-06-05 22:34:40 +0200

    Created person cards using css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import Person from './components/Person'
 import { useState } from 'react'
+import './App.css'
 
 function App() {
   //let name = "Lulamile"
   const [name, setName] = useState('Lulamile');
   return (
-    <div className='App'>
-      <input type="text" onChange={(e) => {setName(e.target.value)}}/>
-      <Person name={name} role="Student"/>
-      <Person name="Benni"/>
-      <Person/>
+    <div className='container'>
+      <div className='App'>
+        {/*<input type="text" onChange={(e) => {setName(e.target.value)}}/>*/}
+        <Person name={name} role="Student"/>
+        <Person name="Benni"/>
+        <Person/>
+        <Person name="Benni"/>
+        <Person/>
+        <Person/>
+      </div>
+        <>
+          <button className='add-new-employee'>+ New Person</button>
+        </>
     </div>
   )
 }
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,9 +1,16 @@
+import './Person.css'
+import image from '../essentials/Vini.jpg';
+
 function Person(props){
     return(
-        <>
-            <h1>This is {props.name?props.name:"Unkown"}</h1>
-            <h2>Role: {props.role?props.role:"No role"}</h2>
-        </>
+        <div className='card'>
+            <img src={image} alt='user image'/>
+            <div className='card-text'>
+                <h3>{props.name?props.name:"Unkown"}</h3>
+                <p>{props.role?props.role:"No role"}</p>
+                <button className='editBtn'>Edit</button>
+            </div>
+        </div>
     )
 }
 
