74d094d5cf17e285848e08db1ed436ea67441fe1
lulamile99 <lulabenni45@gmail.com>
2024-06-05 02:40:51 +0200

    Implementing my first component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Person from './components/Person'
+
+function App() {
+  return (
+    <div>
+      <Person/>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Person.jsx
@@ -0,0 +1,7 @@
+function Person(){
+    return(
+        <h1>This is a Person</h1>
+    )
+}
+
+export default Person;
