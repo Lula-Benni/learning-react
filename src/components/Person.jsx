1c653a747593ea6691296cb4bba389912092a22b
Lula-Benni <lulabenni45@gmail.com>
2024-06-24 19:10:18 +0200

    Used Tailwind instead of plain css

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,10 +1,8 @@
-import './Person.css'
-import image from '../essentials/Vini.jpg';
 import EditPerson from './EditPerson';
 
 function Person(props){
     return(
-        <div className='card'>
+        /**<div className='card'>
             <img className="profile-image" src={props.img} alt='user image'/>
             <div className='card-text'>
                 <h3 className='profile-name'>{props.name}</h3>
@@ -12,6 +10,21 @@ function Person(props){
                 <EditPerson id={props.id} name={props.name} role={props.role} 
                 updatePerson={props.updatePerson}/>
             
+            </div>
+        </div>*/
+        <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+            <img className="block w-24 h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt='user image'/>
+            <div className="text-center space-y-2 sm:text-left">
+                <div className="space-y-0.5">
+                <p className="text-lg text-black font-semibold">
+                    {props.name}
+                </p>
+                <p className="text-slate-500 font-medium">
+                    {props.role}
+                </p>
+                </div>
+                <EditPerson id={props.id} name={props.name} role={props.role} 
+                updatePerson={props.updatePerson}/>
             </div>
         </div>
     )
